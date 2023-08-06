38604498fa34ae7c626b310958938d17d90b721a
Yuanchao Sun <yuanchao.sun@gmail.com>
2023-08-06 22:28:55 +0800

    Update

--- modules/core/03-connection/keeper/handshake.go
+++ modules/core/03-connection/keeper/handshake.go
@@ -85,23 +85,23 @@ func (k Keeper) ConnOpenTry(
 
 	// check that the consensus height the counterparty chain is using to store a representation
 	// of this chain's consensus state is at a height in the past
-	selfHeight := clienttypes.GetSelfHeight(ctx)
-	if consensusHeight.GTE(selfHeight) {
-		return "", sdkerrors.Wrapf(
-			sdkerrors.ErrInvalidHeight,
-			"consensus height is greater than or equal to the current block height (%s >= %s)", consensusHeight, selfHeight,
-		)
-	}
+	// selfHeight := clienttypes.GetSelfHeight(ctx)
+	// if consensusHeight.GTE(selfHeight) {
+	// 	return "", sdkerrors.Wrapf(
+	// 		sdkerrors.ErrInvalidHeight,
+	// 		"consensus height is greater than or equal to the current block height (%s >= %s)", consensusHeight, selfHeight,
+	// 	)
+	// }
 
-	// validate client parameters of a chainB client stored on chainA
-	if err := k.clientKeeper.ValidateSelfClient(ctx, clientState); err != nil {
-		return "", err
-	}
+	// // validate client parameters of a chainB client stored on chainA
+	// if err := k.clientKeeper.ValidateSelfClient(ctx, clientState); err != nil {
+	// 	return "", err
+	// }
 
-	expectedConsensusState, err := k.clientKeeper.GetSelfConsensusState(ctx, consensusHeight)
-	if err != nil {
-		return "", sdkerrors.Wrapf(err, "self consensus state not found for height %s", consensusHeight.String())
-	}
+	// expectedConsensusState, err := k.clientKeeper.GetSelfConsensusState(ctx, consensusHeight)
+	// if err != nil {
+	// 	return "", sdkerrors.Wrapf(err, "self consensus state not found for height %s", consensusHeight.String())
+	// }
 
 	// expectedConnection defines Chain A's ConnectionEnd
 	// NOTE: chain A's counterparty is chain B (i.e where this code is executed)
@@ -135,11 +135,11 @@ func (k Keeper) ConnOpenTry(
 	}
 
 	// Check that ChainA stored the correct ConsensusState of chainB at the given consensusHeight
-	if err := k.VerifyClientConsensusState(
-		ctx, connection, proofHeight, consensusHeight, proofConsensus, expectedConsensusState,
-	); err != nil {
-		return "", err
-	}
+	// if err := k.VerifyClientConsensusState(
+	// 	ctx, connection, proofHeight, consensusHeight, proofConsensus, expectedConsensusState,
+	// ); err != nil {
+	// 	return "", err
+	// }
 
 	// store connection in chainB state
 	if err := k.addConnectionToClient(ctx, clientID, connectionID); err != nil {
@@ -176,13 +176,13 @@ func (k Keeper) ConnOpenAck(
 ) error {
 	// check that the consensus height the counterparty chain is using to store a representation
 	// of this chain's consensus state is at a height in the past
-	selfHeight := clienttypes.GetSelfHeight(ctx)
-	if consensusHeight.GTE(selfHeight) {
-		return sdkerrors.Wrapf(
-			sdkerrors.ErrInvalidHeight,
-			"consensus height is greater than or equal to the current block height (%s >= %s)", consensusHeight, selfHeight,
-		)
-	}
+	// selfHeight := clienttypes.GetSelfHeight(ctx)
+	// if consensusHeight.GTE(selfHeight) {
+	// 	return sdkerrors.Wrapf(
+	// 		sdkerrors.ErrInvalidHeight,
+	// 		"consensus height is greater than or equal to the current block height (%s >= %s)", consensusHeight, selfHeight,
+	// 	)
+	// }
 
 	// Retrieve connection
 	connection, found := k.GetConnection(ctx, connectionID)
@@ -207,15 +207,15 @@ func (k Keeper) ConnOpenAck(
 	}
 
 	// validate client parameters of a chainA client stored on chainB
-	if err := k.clientKeeper.ValidateSelfClient(ctx, clientState); err != nil {
-		return err
-	}
+	// if err := k.clientKeeper.ValidateSelfClient(ctx, clientState); err != nil {
+	// 	return err
+	// }
 
-	// Retrieve chainA's consensus state at consensusheight
-	expectedConsensusState, err := k.clientKeeper.GetSelfConsensusState(ctx, consensusHeight)
-	if err != nil {
-		return sdkerrors.Wrapf(err, "self consensus state not found for height %s", consensusHeight.String())
-	}
+	// // Retrieve chainA's consensus state at consensusheight
+	// expectedConsensusState, err := k.clientKeeper.GetSelfConsensusState(ctx, consensusHeight)
+	// if err != nil {
+	// 	return sdkerrors.Wrapf(err, "self consensus state not found for height %s", consensusHeight.String())
+	// }
 
 	prefix := k.GetCommitmentPrefix()
 	expectedCounterparty := types.NewCounterparty(connection.ClientId, connectionID, commitmenttypes.NewMerklePrefix(prefix.Bytes()))
@@ -230,16 +230,16 @@ func (k Keeper) ConnOpenAck(
 	}
 
 	// Check that ChainB stored the clientState provided in the msg
-	if err := k.VerifyClientState(ctx, connection, proofHeight, proofClient, clientState); err != nil {
-		return err
-	}
+	// if err := k.VerifyClientState(ctx, connection, proofHeight, proofClient, clientState); err != nil {
+	// 	return err
+	// }
 
-	// Ensure that ChainB has stored the correct ConsensusState for chainA at the consensusHeight
-	if err := k.VerifyClientConsensusState(
-		ctx, connection, proofHeight, consensusHeight, proofConsensus, expectedConsensusState,
-	); err != nil {
-		return err
-	}
+	// // Ensure that ChainB has stored the correct ConsensusState for chainA at the consensusHeight
+	// if err := k.VerifyClientConsensusState(
+	// 	ctx, connection, proofHeight, consensusHeight, proofConsensus, expectedConsensusState,
+	// ); err != nil {
+	// 	return err
+	// }
 
 	k.Logger(ctx).Info("connection state updated", "connection-id", connectionID, "previous-state", "INIT", "new-state", "OPEN")
 
